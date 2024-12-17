20cca246ab6b1cd6c4ed50cba188a3fd144e51c4
SarmatShabdanov <shabdanov_s@auca.kg>
2024-12-17 15:56:17 +0600

    blogger_search

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from left_menu import create_left_menu,left_menu_create_script
 from header import create_header
 from dotenv import load_dotenv
 import os
+import sqlite3
 
 # Load environment variables from .env file
 load_dotenv()
@@ -11,6 +12,36 @@ load_dotenv()
 # Access environment variables
 PORT_ENV = os.getenv('PORT')
 print(PORT_ENV)
+
+def get_bloggers():
+    conn = sqlite3.connect("ad_sphere.db")
+    cursor = conn.cursor()
+    q = "SELECT * from bloggers"
+    cursor.execute(q)
+    result = cursor.fetchall()
+    conn.close()
+    return result 
+
+def get_history():
+    conn = sqlite3.connect("ad_sphere.db")
+    cursor = conn.cursor()
+    q = "SELECT * from history INNER JOIN bloggers ON bloggers.id = history.blogger_id "
+    cursor.execute(q)
+    result = cursor.fetchall()
+    conn.close()
+    return result 
+
+
+
+def get_favorite():
+    conn = sqlite3.connect("ad_sphere.db")
+    cursor = conn.cursor()
+    q = "SELECT * from favorite INNER JOIN bloggers ON bloggers.id = favorite.blogger_id "
+    cursor.execute(q)
+    result = cursor.fetchall()
+    conn.close()
+    return result 
+
 
 app = Flask(__name__)
 @app.route("/")
@@ -25,7 +56,8 @@ def search():
     left_menu = create_left_menu()
     left_menu_script = left_menu_create_script()
     header = create_header()
-    return render_template('./search.html',left_menu = left_menu,left_menu_script = left_menu_script,header = header)
+    bloggers = get_bloggers()
+    return render_template('./search.html',left_menu = left_menu,left_menu_script = left_menu_script,header = header,bloggers = bloggers)
 
 @app.route("/profile")
 def profile():
@@ -47,11 +79,10 @@ def favorite():
 def add_favorite():
     blogger_id = request.form.get('bloggerId')
     print(blogger_id)
-    left_menu = create_left_menu()
-    left_menu_script = left_menu_create_script()
-    header = create_header()
+
     
-    # return render_template("favorite.html",left_menu = left_menu,left_menu_script = left_menu_script,header = header,)  
+    
+    
 @app.route("/sign")
 def sign():
     return render_template('./sign.html')
